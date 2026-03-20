4dfb377a7a9dacffad862356eca09cda2c2e2fec
Maffile-Enky <2309832453@qq.com>
2026-03-20 22:24:58 +0800

    deat：video上传接口检验

--- video-service/src/main/java/com/iot/tiktok/config/AliyunOSSOperator.java
+++ video-service/src/main/java/com/iot/tiktok/config/AliyunOSSOperator.java
@@ -1,17 +1,12 @@
 package com.iot.tiktok.config;
 
-
 import com.aliyun.oss.*;
-import com.aliyun.oss.common.auth.EnvironmentVariableCredentialsProvider;
-import com.aliyun.oss.common.comm.SignVersion;
+import com.aliyun.oss.common.auth.DefaultCredentialProvider;
 import com.aliyun.oss.model.PutObjectRequest;
-import com.aliyun.oss.model.PutObjectResult;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.UUID;
@@ -20,66 +15,41 @@ import java.util.UUID;
 public class AliyunOSSOperator {
 
     @Value("${aliyun.oss.endpoint}")
-    private String endpoint ;
+    private String endpoint;
+
     @Value("${aliyun.oss.bucketName}")
-    private String bucketName ;
-    @Value("${aliyun.oss.region}")
-    private String region ;
+    private String bucketName;
+
+    @Value("${aliyun.oss.accessKeyId}")
+    private String accessKeyId;
+
+    @Value("${aliyun.oss.accessKeySecret}")
+    private String accessKeySecret;
 
     public String upload(byte[] content, String originalFilename) throws Exception {
-        //从环境变量中获取访问凭证
-        EnvironmentVariableCredentialsProvider credentialsProvider = new EnvironmentVariableCredentialsProvider();
-
-        //目录名，获取当前系统日期的字符串
+        // 按日期生成目录
         String dir = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM"));
-
-        //生成一个新的不重复的文件名
-        String newFileName = UUID.randomUUID() + originalFilename.substring(originalFilename.lastIndexOf("."));
-
-        //新的对象名
+        String suffix = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String newFileName = UUID.randomUUID() + suffix;
         String objectName = dir + "/" + newFileName;
 
-        // 创建OSSClient实例。
-        // 当OSSClient实例不再使用时，调用shutdown方法以释放资源。
-        ClientBuilderConfiguration clientBuilderConfiguration = new ClientBuilderConfiguration();
-        clientBuilderConfiguration.setSignatureVersion(SignVersion.V4);
-        OSS ossClient = OSSClientBuilder.create()
-                .endpoint(endpoint)
-                .credentialsProvider(credentialsProvider)
-                .clientConfiguration(clientBuilderConfiguration)
-                .region(region)
-                .build();
+
+        OSS ossClient = new OSSClientBuilder().build(
+                endpoint,
+                accessKeyId,
+                accessKeySecret
+        );
 
         try {
-            // 创建PutObjectRequest对象。
-            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, new ByteArrayInputStream(content));
-            // 如果需要上传时设置存储类型和访问权限，请参考以下示例代码。
-            // ObjectMetadata metadata = new ObjectMetadata();
-            // metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
-            // metadata.setObjectAcl(CannedAccessControlList.Private);
-            // putObjectRequest.setMetadata(metadata);
-
-            // 上传文件。
-            PutObjectResult result = ossClient.putObject(putObjectRequest);
-        } catch (OSSException oe) {
-            System.out.println("Caught an OSSException, which means your request made it to OSS, "
-                    + "but was rejected with an error response for some reason.");
-            System.out.println("Error Message:" + oe.getErrorMessage());
-            System.out.println("Error Code:" + oe.getErrorCode());
-            System.out.println("Request ID:" + oe.getRequestId());
-            System.out.println("Host ID:" + oe.getHostId());
-        } catch (ClientException ce) {
-            System.out.println("Caught an ClientException, which means the client encountered "
-                    + "a serious internal problem while trying to communicate with OSS, "
-                    + "such as not being able to access the network.");
-            System.out.println("Error Message:" + ce.getMessage());
+            // 上传
+            ossClient.putObject(bucketName, objectName, new ByteArrayInputStream(content));
         } finally {
             if (ossClient != null) {
                 ossClient.shutdown();
             }
         }
-        return endpoint.split("//")[0] + "//" + bucketName + "." + endpoint.split("//")[1] + "/" + objectName;
+
+        // 返回正确的访问地址
+        return "https://" + bucketName + "." + endpoint + "/" + objectName;
     }
-
-
 }
